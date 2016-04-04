995764949034f0623b95089732be3be7702915ea
Casper da Costa-Luis <casper.dcl@physics.org>
2016-04-05 00:36:36 +0100

    neaten tabulate

--- gitfame/_gitfame.py
+++ gitfame/_gitfame.py
@@ -7,7 +7,7 @@ Options:
   -h, --help     Print this help and exit.
   -v, --version  Print module version and exit.
   --sort=<key>    Options: [default: loc], files, commits.
-  --excl=<f>      Excluded files [default: None].
+  --excl=<f>      Excluded files (default: None).
                   In no-regex mode, may be a comma-separated list.
                   Escape (\,) for a literal comma (may require \\, in shell).
   --incl=<f>      Included files [default: .*]. See `--excl` for format.
@@ -29,6 +29,11 @@ from __future__ import division
 # from __future__ import absolute_import
 import subprocess
 import re
+try:  # pragma: no cover
+  from tabulate import tabulate as tabber
+except ImportError:  # pragma: no cover
+  tabber = None
+
 from ._utils import TERM_WIDTH, int_cast_or_len, Max, fext, _str, tqdm
 
 __author__ = "Casper da Costa-Luis <casper@caspersci.uk.to>"
@@ -51,46 +56,27 @@ def tr_hline(col_widths, hl='-', x='+'):
   return x + x.join(hl * i for i in col_widths) + x
 
 
-def blank_col(rows, i, blanks=' +=-'):
-  lenR = len(rows[0])
-  return all(r[i] in blanks for r in rows)
-
-
-def tighten(t):
-  """ Tighten grid table padding """
-  rows = t.strip().split('\n')
-  i = 1
-  curr_blank = bool()
-  prev_blank = blank_col(rows, i - 1)
-  while (i < len(rows[0])):
-    curr_blank = blank_col(rows, i)
-    if prev_blank and curr_blank:
-      rows = [r[:i - 1] + r[i:] for r in rows]
-      i -= 1
-    prev_blank = curr_blank
-    i += 1
-  return '\n'.join(rows)
-
-
-def tabulate(auth_stats, stats_tot, args_sort="loc", args_bytype=False):
+def tabulate(auth_stats, stats_tot, args_sort='loc', args_bytype=False):
+  COL_NAMES = ['Author', 'loc', 'coms', 'fils', ' distribution']
   it_as = getattr(auth_stats, 'iteritems', auth_stats.items)
-  tab = [[auth, s['loc'], s.get('commits', 0), len(s.get('files', [])),
+  # get ready
+  tab = [[auth,
+          s['loc'],
+          s.get('commits', 0),
+          len(s.get('files', [])),
           '/'.join(map('{0:4.1f}'.format, (
-              100 * s['loc'] / max(1, stats_tot["loc"]),
-              100 * s.get('commits', 0) / max(1, stats_tot["commits"]),
-              100 * len(s.get('files', [])) / max(1, stats_tot["files"])))
-              ).replace('/100.0/', '/ 100/')
-]
+              100 * s['loc'] / max(1, stats_tot['loc']),
+              100 * s.get('commits', 0) / max(1, stats_tot['commits']),
+              100 * len(s.get('files', [])) / max(1, stats_tot['files'])
+          ))).replace('/100.0/', '/ 100/')]
          for (auth, s) in sorted(it_as(),
-             key=lambda k: int_cast_or_len(k[1].get(args_sort, 0)),
-             reverse=True)]
-  try:
-    from tabulate import tabulate as tabber
-    return tighten(tabber(tab,
-                          ['Author', 'loc', 'coms', 'fils', '  distribution'],
-                          tablefmt='grid', floatfmt='.0f'))
-  except ImportError:
-    pass
+         key=lambda k: int_cast_or_len(k[1].get(args_sort, 0)),
+         reverse=True)]
+
+  if tabber is not None:
+    from ._utils import tighten
+    return tighten(tabber(tab, COL_NAMES, tablefmt='grid', floatfmt='.0f'),
+                   max_width=TERM_WIDTH)
 
   res = ''
   it_val_as = getattr(auth_stats, 'itervalues', auth_stats.values)
@@ -160,7 +146,8 @@ def run(args):
     # sys.argv[0][:sys.argv[0].replace('\\','/').rfind('/')]
 
   if args["--sort"] not in ["loc", "commits", "files"]:
-    raise(Warning("--sort argument unrecognised\n" + __doc__))
+    raise(Warning("--sort argument (" + args["--sort"] +
+                  ") unrecognised\n" + __doc__))
 
   if not args["--excl"]:
     args["--excl"] = ""
@@ -200,7 +187,7 @@ def run(args):
   else:
     file_list = [i for i in file_list
                  if include_files.search(i)
-                 if not exclude_files.search(i)]
+                 if not (args["--excl"] and exclude_files.search(i))]
   # print(file_list)
 
   auth_stats = {}
@@ -279,5 +266,5 @@ def main():
   run(args)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
   main()
