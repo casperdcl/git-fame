3bd9cfeef09b06c40529ad8aa30968b305be716b
Marcus Medley <mdmedley@users.noreply.github.com>
2017-03-29 10:00:34 -0700

    Add missing tqdm requirement
    This PR adds tqdm to the requirements so the user doesn't run into "warning | module tqdm not found"

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     platforms=['any'],
     packages=['gitfame'],
     provides=['gitfame'],
-    install_requires=['docopt'],
+    install_requires=['docopt', 'tqdm'],
     entry_points={'console_scripts': ['git-fame=gitfame:main'], },
     ext_modules=cythonize(["gitfame/_gitfame.py", "gitfame/_utils.py"],
                           nthreads=2),
