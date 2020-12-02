1eb53423ded123696cbebeb6a248f97698866a76
Casper da Costa-Luis <casper.dcl@physics.org>
2020-12-02 18:44:39 +0000

    minor build upgrades

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from ast import literal_eval
-from io import open as io_open
 from os import path
 from setuptools import setup
 import sys
@@ -24,19 +22,4 @@ if '--cython' in sys.argv:
     except ImportError:
         pass
 
-__author__ = None
-__licence__ = None
-for line in io_open(path.join(src_dir, 'gitfame', '_gitfame.py'), mode='r'):
-    if line.startswith('__author__'):
-        __author__ = literal_eval(line.split('=', 1)[1].strip())
-    elif line.startswith('__licence__'):
-        __licence__ = literal_eval(line.split('=', 1)[1].strip())
-
-setup(
-    use_scm_version=True,
-    license=__licence__.lstrip('[').split(']')[0],
-    author=__author__.split('<')[0].strip(),
-    author_email=__author__.split('<')[1][:-1],
-    test_suite='nose.collector',
-    ext_modules=ext_modules,
-)
+setup(use_scm_version=True, test_suite='nose.collector', ext_modules=ext_modules)
